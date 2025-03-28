e82e59d516f4b879f10527983ddbeeb4f79aad41
jnicolasdz <jndiazs@udistrital.edu.co>
2025-03-28 12:29:49 -0500

    adding alias to rc

--- install.sh
+++ install.sh
@@ -6,10 +6,9 @@
 # under these dir, create script to start Astrometrica + Alias 
 
 echo "Get in astrometrica set up exe"
-source="http://iasc.cosmosearch.org/Content/Distributables/astrometrica-setup-v1.4.1.exe"
-version="${source##*/}"
-curl -O "$source"
-
+link="http://iasc.cosmosearch.org/Content/Distributables/astrometrica-setup-v1.4.1.exe"
+version="${link##*/}"
+curl -O "$link"
 
 echo "Download wine"
 git clone https://gitlab.winehq.org/wine/wine.git
@@ -20,4 +19,7 @@ make install
 
 echo "Install astrometrica in wine"
 wine $version
+echo "# Alias for Astrometrica" >> $HOME/.bashrc
+echo "alias astrometrica='cd $HOME/.wine/dosdevices/c:/Astrometrica && wine cmd /c Astrometrica.bat && exit' " >> $HOME/.bashrc
+
 
